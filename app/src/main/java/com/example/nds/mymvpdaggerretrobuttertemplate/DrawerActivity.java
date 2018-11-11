f06d45ffa441a9692d425f20092b005fc7f7dad8
NDS <dddddd1997@gmail.com>
2018-11-12 02:25:22 +0300

    start

---
+++ app/src/main/java/com/example/nds/mymvpdaggerretrobuttertemplate/DrawerActivity.java
@@ -0,0 +1,6 @@
+package com.example.nds.mymvpdaggerretrobuttertemplate;
+
+import com.example.nds.mymvpdaggerretrobuttertemplate.base.BaseActivity;
+
+public class DrawerActivity extends BaseActivity {
+}
---
+++ app/src/main/java/com/example/nds/mymvpdaggerretrobuttertemplate/base/BaseActivity.java
@@ -0,0 +1,6 @@
+package com.example.nds.mymvpdaggerretrobuttertemplate.base;
+
+import android.app.Activity;
+
+public class BaseActivity extends Activity {
+}
